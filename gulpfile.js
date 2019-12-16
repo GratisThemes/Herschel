dc32e1ca7112e624a343bd5de46935b740ffbf60
Kniffen <knifftech@gmail.com>
2019-12-16 16:40:23 +0100

    Updated dev process

--- gulpfile.js
+++ gulpfile.js
@@ -5,62 +5,57 @@ const prefix  = require( 'gulp-autoprefixer' )
 const wpPot   = require( 'gulp-wp-pot' )
 const sort    = require( 'gulp-sort' )
 const zip     = require( 'gulp-zip' )
+const pkg     = require("./package.json")
 
-const info = {
-  name:      'Herschel',
-  slug:      'herschel',
-  version:   '1.6.0',
-  author:    'GratisThemes',
-  email:     'gratisthemes@gmail.com',
-  bugReport: 'https://github.com/GratisThemes/Fell/issues'
+function css() {
+  return gulp.src("./scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass({outputStyle: "expanded", includePaths: ["scss"]}))
+    .pipe(prefix(["last 30 versions", "> 1%", "ie 8", "ie 7"], {cascade: true}))
+    .pipe(gulp.dest("./"))
 }
 
-// CSS
-gulp.task( 'css', () => {
-  gulp.src('./scss/*.scss')
-    .pipe(plumber())
-    .pipe(sass({ outputStyle: 'expanded', includePaths: ['scss'] }))
-    .pipe(prefix(['last 30 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-    .pipe(gulp.dest('./'))
-} )
-
-// Pot
-gulp.task('pot', () => {
-  gulp.src('./**/*.php')
+function pot() {
+  return gulp.src("./**/*.php")
     .pipe(plumber())
     .pipe(sort())
     .pipe(wpPot({
-        domain: info.slug,
-        package: info.slug,
-        bugReport: info.bugReport,
-        lastTranslator: `${info.author} <${info.email}>`,
-        team: `${info.author} <${info.email}>`
+        domain:         pkg.name,
+        package:        pkg.name,
+        bugReport:      pkg.bugs.url,
+        lastTranslator: pkg.author,
+        team:           pkg.author
     }))
-    .pipe(gulp.dest(`./languages/${info.slug}.pot`))
-})
+    .pipe(gulp.dest(`./languages/${pkg.name}.pot`))
+}
 
-// Package
-gulp.task( 'package', () => {
-  gulp.src( [
-    './**/*.*',
-    '!./.git',
-    '!./node_modules/**/*.*',
-    '!./releases/**/*.*',
-    '!./scss/**/*.*',
-    '!./.gitignore',
-    '!./gulpfile.js',
-    '!./package.json',
-    '!./package-lock.json',
+function dist() {
+  return gulp.src([
+    "./**/*.*",
+    "!./.git",
+    "!./.gitignore",
+    "!./releases/**/*.*",
+    "!./gulpfile.js",
+    "!./node_modules/**/*.*",
+    "!./package-lock.json",
+    "!./package.json",    
+    "!./scss/**/*.*",
   ], {
-    base: '..'
-  } ).pipe( zip( `${info.slug}_${info.version}.zip` ) )
-     .pipe( gulp.dest( './releases' ) )
-} )
+    base: ".."
+  }).pipe(zip(`${pkg.name}_${pkg.version}.zip`))
+    .pipe(gulp.dest("./releases"))
+}
 
-// Watch
-gulp.task('watch', () => {
-  gulp.watch( 'scss/*.scss', { cwd: './' }, [ 'css' ] )
-  gulp.watch( '**/*.php',           { cwd: './' }, [ 'pot' ] )
-})
+function watch(done) {
+  gulp.watch("scss/**/*.scss", {cwd: "./", usePolling: true}, css)
+  gulp.watch("**/*.php",       {cwd: "./", usePolling: true}, pot)
 
-gulp.task('default', [ 'watch', 'css', 'pot' ]);
+  return done()
+}
+
+module.exports.css   = css
+module.exports.pot   = pot
+module.exports.dist  = dist
+module.exports.watch = watch
+
+module.exports.default = gulp.series(pot, css, watch)
